1a72bc4ea725f98fb3bec4616289dfa01652b999
John P. Bloch <johnpbloch@gmail.com>
2014-03-19 02:01:15 -0400

    Update trunk

--- wp-admin/includes/post.php
+++ wp-admin/includes/post.php
@@ -1379,11 +1379,13 @@ function post_preview() {
 	$post_ID = (int) $_POST['post_ID'];
 	$_POST['ID'] = $post_ID;
 
-	if ( ! $post = get_post( $post_ID ) )
-		wp_die( __('You attempted to preview a non existing item.') );
+	if ( ! $post = get_post( $post_ID ) ) {
+		wp_die( __( 'You are not allowed to edit this post.' ) );
+	}
 
-	if ( ! current_user_can( 'edit_post', $post->ID ) )
-		wp_die( __('You are not allowed to preview this item.') );
+	if ( ! current_user_can( 'edit_post', $post->ID ) ) {
+		wp_die( __( 'You are not allowed to edit this post.' ) );
+	}
 
 	$is_autosave = false;
 
@@ -1434,13 +1436,15 @@ function wp_autosave( $post_data ) {
 	$post_id = (int) $post_data['post_id'];
 	$post_data['ID'] = $post_data['post_ID'] = $post_id;
 
-	if ( false === wp_verify_nonce( $post_data['_wpnonce'], 'update-post_' . $post_id ) )
-		return new WP_Error( 'invalid_nonce', __('ERROR: invalid post data.') );
+	if ( false === wp_verify_nonce( $post_data['_wpnonce'], 'update-post_' . $post_id ) ) {
+		return new WP_Error( 'invalid_nonce', __( 'Error while saving.' ) );
+	}
 
 	$post = get_post( $post_id );
 
-	if ( ! current_user_can( 'edit_post', $post->ID ) )
-		return new WP_Error( 'edit_post', __('You are not allowed to edit this item.') );
+	if ( ! current_user_can( 'edit_post', $post->ID ) ) {
+		return new WP_Error( 'edit_posts', __( 'You are not allowed to edit this item.' ) );
+	}
 
 	if ( 'auto-draft' == $post->post_status )
 		$post_data['post_status'] = 'draft';
