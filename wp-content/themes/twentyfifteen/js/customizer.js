a2f425a9eaabf4ea5acf82593ffe31bf5d0d5f70
John P. Bloch <johnpbloch@gmail.com>
2014-10-24 23:46:08 -0400

    Update trunk

---
+++ wp-content/themes/twentyfifteen/js/customizer.js
@@ -0,0 +1,19 @@
+/**
+ * Customizer enhancements for a better user experience.
+ *
+ * Contains handlers to make Customizer preview reload changes asynchronously.
+ */
+
+( function( $ ) {
+	// Site title and description.
+	wp.customize( 'blogname', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-title a' ).text( to );
+		} );
+	} );
+	wp.customize( 'blogdescription', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-description' ).text( to );
+		} );
+	} );
+} )( jQuery );
