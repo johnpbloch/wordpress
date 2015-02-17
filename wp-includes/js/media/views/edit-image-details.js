4ac47981d4136f1e62b804ae71f1c1158a0c906d
John P. Bloch <johnpbloch@gmail.com>
2015-02-16 20:01:19 -0500

    Update trunk

---
+++ wp-includes/js/media/views/edit-image-details.js
@@ -0,0 +1,24 @@
+var View = require( './view.js' ),
+	EditImage = require( './edit-image.js' ),
+	Details;
+
+Details = EditImage.extend({
+	initialize: function( options ) {
+		this.editor = window.imageEdit;
+		this.frame = options.frame;
+		this.controller = options.controller;
+		View.prototype.initialize.apply( this, arguments );
+	},
+
+	back: function() {
+		this.frame.content.mode( 'edit-metadata' );
+	},
+
+	save: function() {
+		this.model.fetch().done( _.bind( function() {
+			this.frame.content.mode( 'edit-metadata' );
+		}, this ) );
+	}
+});
+
+module.exports = Details;
