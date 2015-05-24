363ec65bbbf87a652e31e68d782794b98549db2d
John P. Bloch <johnpbloch@gmail.com>
2015-05-24 04:01:29 -0400

    Update trunk

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.3-alpha-32568';
+$wp_version = '4.3-alpha-32569';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
