663de323feee4cc1faaace433e43056912fd15b2
John P. Bloch <johnpbloch@gmail.com>
2015-07-19 07:16:13 -0400

    Update trunk

---
+++ wp-admin/js/word-count.js
@@ -0,0 +1,103 @@
+( function() {
+	function WordCounter( settings ) {
+		var key,
+			shortcodes;
+
+		if ( settings ) {
+			for ( key in settings ) {
+				if ( settings.hasOwnProperty( key ) ) {
+					this.settings[ key ] = settings[ key ];
+				}
+			}
+		}
+
+		shortcodes = this.settings.l10n.shortcodes;
+
+		if ( shortcodes && shortcodes.length ) {
+			this.settings.shortcodesRegExp = new RegExp( '\\[\\/?(?:' + shortcodes.join( '|' ) + ')[^\\]]*?\\]', 'gi' );
+		}
+	}
+
+	WordCounter.prototype.settings = {
+		HTMLRegExp: /<\/?[a-z][^>]*?>/gi,
+		spaceRegExp: /&nbsp;|&#160;/gi,
+		connectorRegExp: /--|\u2014/gi,
+		removeRegExp: new RegExp( [
+			'[',
+				// Basic Latin (extract)
+				'\u0021-\u0040\u005B-\u0060\u007B-\u007E',
+				// Latin-1 Supplement (extract)
+				'\u0080-\u00BF\u00D7\u00F7',
+				// General Punctuation
+				// Superscripts and Subscripts
+				// Currency Symbols
+				// Combining Diacritical Marks for Symbols
+				// Letterlike Symbols
+				// Number Forms
+				// Arrows
+				// Mathematical Operators
+				// Miscellaneous Technical
+				// Control Pictures
+				// Optical Character Recognition
+				// Enclosed Alphanumerics
+				// Box Drawing
+				// Block Elements
+				// Geometric Shapes
+				// Miscellaneous Symbols
+				// Dingbats
+				// Miscellaneous Mathematical Symbols-A
+				// Supplemental Arrows-A
+				// Braille Patterns
+				// Supplemental Arrows-B
+				// Miscellaneous Mathematical Symbols-B
+				// Supplemental Mathematical Operators
+				// Miscellaneous Symbols and Arrows
+				'\u2000-\u2BFF',
+				// Supplemental Punctuation
+				'\u2E00-\u2E7F',
+			']'
+		].join( '' ), 'g' ),
+		astralRegExp: /[\uD800-\uDBFF][\uDC00-\uDFFF]/g,
+		wordsRegExp: /\S\s+/g,
+		charactersRegExp: /\S/g,
+		allRegExp: /[^\f\n\r\t\v\u00ad\u2028\u2029]/g,
+		l10n: window.wordCountL10n || {}
+	};
+
+	WordCounter.prototype.count = function( text, type ) {
+		var count = 0;
+
+		type = type || this.settings.l10n.type || 'words';
+
+		if ( text ) {
+			text = text + '\n';
+
+			text = text.replace( this.settings.HTMLRegExp, '\n' );
+
+			if ( this.settings.shortcodesRegExp ) {
+				text = text.replace( this.settings.shortcodesRegExp, '\n' );
+			}
+
+			text = text.replace( this.settings.spaceRegExp, ' ' );
+
+			if ( type === 'words' ) {
+				text = text.replace( this.settings.connectorRegExp, ' ' );
+				text = text.replace( this.settings.removeRegExp, '' );
+			} else {
+				text = text.replace( this.settings.astralRegExp, 'a' );
+			}
+
+			text = text.match( this.settings[ type + 'RegExp' ] );
+
+			if ( text ) {
+				count = text.length;
+			}
+		}
+
+		return count;
+	};
+
+	window.wp = window.wp || {};
+	window.wp.utils = window.wp.utils || {};
+	window.wp.utils.WordCounter = WordCounter;
+} )();
---
+++ wp-includes/js/customize-preview-nav-menus.js
@@ -0,0 +1,252 @@
+/*global jQuery, JSON, _wpCustomizePreviewNavMenusExports, _ */
+
+wp.customize.menusPreview = ( function( $, api ) {
+	'use strict';
+	var self;
+
+	self = {
+		renderQueryVar: null,
+		renderNonceValue: null,
+		renderNoncePostKey: null,
+		previewCustomizeNonce: null,
+		requestUri: '/',
+		theme: {
+			active: false,
+			stylesheet: ''
+		},
+		navMenuInstanceArgs: {},
+		refreshDebounceDelay: 200
+	};
+
+	api.bind( 'preview-ready', function() {
+		api.preview.bind( 'active', function() {
+			self.init();
+		} );
+	} );
+
+	/**
+	 * Bootstrap functionality.
+	 */
+	self.init = function() {
+		var self = this;
+
+		if ( 'undefined' !== typeof _wpCustomizePreviewNavMenusExports ) {
+			$.extend( self, _wpCustomizePreviewNavMenusExports );
+		}
+
+		api.each( function( setting, id ) {
+			setting.id = id;
+			self.bindListener( setting );
+		} );
+
+		api.preview.bind( 'setting', function( args ) {
+			var id, value, setting;
+			args = args.slice();
+			id = args.shift();
+			value = args.shift();
+			if ( ! api.has( id ) ) {
+				// Currently customize-preview.js is not creating settings for dynamically-created settings in the pane; so we have to do it
+				setting = api.create( id, value ); // @todo This should be in core
+				setting.id = id;
+				if ( self.bindListener( setting ) ) {
+					setting.callbacks.fireWith( setting, [ setting(), null ] );
+				}
+			}
+		} );
+	};
+
+	/**
+	 *
+	 * @param {wp.customize.Value} setting
+	 * @returns {boolean} Whether the setting was bound.
+	 */
+	self.bindListener = function( setting ) {
+		var matches, themeLocation;
+
+		matches = setting.id.match( /^nav_menu\[(-?\d+)]$/ );
+		if ( matches ) {
+			setting.navMenuId = parseInt( matches[1], 10 );
+			setting.bind( self.onChangeNavMenuSetting );
+			return true;
+		}
+
+		matches = setting.id.match( /^nav_menu_item\[(-?\d+)]$/ );
+		if ( matches ) {
+			setting.navMenuItemId = parseInt( matches[1], 10 );
+			setting.bind( self.onChangeNavMenuItemSetting );
+			return true;
+		}
+
+		matches = setting.id.match( /^nav_menu_locations\[(.+?)]/ );
+		if ( matches ) {
+			themeLocation = matches[1];
+			setting.bind( function() {
+				self.refreshMenuLocation( themeLocation );
+			} );
+			return true;
+		}
+
+		return false;
+	};
+
+	/**
+	 * Handle changing of a nav_menu setting.
+	 *
+	 * @this {wp.customize.Setting}
+	 */
+	self.onChangeNavMenuSetting = function() {
+		var setting = this;
+		if ( ! setting.navMenuId ) {
+			throw new Error( 'Expected navMenuId property to be set.' );
+		}
+		self.refreshMenu( setting.navMenuId );
+	};
+
+	/**
+	 * Handle changing of a nav_menu_item setting.
+	 *
+	 * @this {wp.customize.Setting}
+	 * @param {object} to
+	 * @param {object} from
+	 */
+	self.onChangeNavMenuItemSetting = function( to, from ) {
+		if ( from && from.nav_menu_term_id && ( ! to || from.nav_menu_term_id !== to.nav_menu_term_id ) ) {
+			self.refreshMenu( from.nav_menu_term_id );
+		}
+		if ( to && to.nav_menu_term_id ) {
+			self.refreshMenu( to.nav_menu_term_id );
+		}
+	};
+
+	/**
+	 * Update a given menu rendered in the preview.
+	 *
+	 * @param {int} menuId
+	 */
+	self.refreshMenu = function( menuId ) {
+		var self = this, assignedLocations = [];
+
+		api.each(function( setting, id ) {
+			var matches = id.match( /^nav_menu_locations\[(.+?)]/ );
+			if ( matches && menuId === setting() ) {
+				assignedLocations.push( matches[1] );
+			}
+		});
+
+		_.each( self.navMenuInstanceArgs, function( navMenuArgs, instanceNumber ) {
+			if ( menuId === navMenuArgs.menu || -1 !== _.indexOf( assignedLocations, navMenuArgs.theme_location ) ) {
+				self.refreshMenuInstanceDebounced( instanceNumber );
+			}
+		} );
+	};
+
+	self.refreshMenuLocation = function( location ) {
+		var foundInstance = false;
+		_.each( self.navMenuInstanceArgs, function( navMenuArgs, instanceNumber ) {
+			if ( location === navMenuArgs.theme_location ) {
+				self.refreshMenuInstanceDebounced( instanceNumber );
+				foundInstance = true;
+			}
+		} );
+		if ( ! foundInstance ) {
+			api.preview.send( 'refresh' );
+		}
+	};
+
+	/**
+	 * Update a specific instance of a given menu on the page.
+	 *
+	 * @param {int} instanceNumber
+	 */
+	self.refreshMenuInstance = function( instanceNumber ) {
+		var self = this, data, menuId, customized, container, request, wpNavArgs, instance, containerInstanceClassName;
+
+		if ( ! self.navMenuInstanceArgs[ instanceNumber ] ) {
+			throw new Error( 'unknown_instance_number' );
+		}
+		instance = self.navMenuInstanceArgs[ instanceNumber ];
+
+		containerInstanceClassName = 'partial-refreshable-nav-menu-' + String( instanceNumber );
+		container = $( '.' + containerInstanceClassName );
+
+		if ( _.isNumber( instance.menu ) ) {
+			menuId = instance.menu;
+		} else if ( instance.theme_location && api.has( 'nav_menu_locations[' + instance.theme_location + ']' ) ) {
+			menuId = api( 'nav_menu_locations[' + instance.theme_location + ']' ).get();
+		}
+
+		if ( ! menuId || ! instance.can_partial_refresh || 0 === container.length ) {
+			api.preview.send( 'refresh' );
+			return;
+		}
+		menuId = parseInt( menuId, 10 );
+
+		data = {
+			nonce: self.previewCustomizeNonce, // for Customize Preview
+			wp_customize: 'on'
+		};
+		if ( ! self.theme.active ) {
+			data.theme = self.theme.stylesheet;
+		}
+		data[ self.renderQueryVar ] = '1';
+		customized = {};
+		api.each( function( setting, id ) {
+			// @todo Core should propagate the dirty state into the Preview as well so we can use that here.
+			if ( id === 'nav_menu[' + String( menuId ) + ']' || ( /^nav_menu_item\[/.test( id ) && setting() && menuId === setting().nav_menu_term_id ) ) {
+				customized[ id ] = setting.get();
+			}
+		} );
+		data.customized = JSON.stringify( customized );
+		data[ self.renderNoncePostKey ] = self.renderNonceValue;
+
+		wpNavArgs = $.extend( {}, instance );
+		data.wp_nav_menu_args_hash = wpNavArgs.args_hash;
+		delete wpNavArgs.args_hash;
+		data.wp_nav_menu_args = JSON.stringify( wpNavArgs );
+
+		container.addClass( 'customize-partial-refreshing' );
+
+		request = wp.ajax.send( null, {
+			data: data,
+			url: self.requestUri
+		} );
+		request.done( function( data ) {
+			// If the menu is now not visible, refresh since the page layout may have changed.
+			if ( false === data ) {
+				api.preview.send( 'refresh' );
+				return;
+			}
+
+			var eventParam, previousContainer = container;
+			container = $( data );
+			container.addClass( containerInstanceClassName );
+			container.addClass( 'partial-refreshable-nav-menu customize-partial-refreshing' );
+			previousContainer.replaceWith( container );
+			eventParam = {
+				instanceNumber: instanceNumber,
+				wpNavArgs: wpNavArgs,
+				oldContainer: previousContainer,
+				newContainer: container
+			};
+			container.removeClass( 'customize-partial-refreshing' );
+			$( document ).trigger( 'customize-preview-menu-refreshed', [ eventParam ] );
+		} );
+	};
+
+	self.currentRefreshMenuInstanceDebouncedCalls = {};
+
+	self.refreshMenuInstanceDebounced = function( instanceNumber ) {
+		if ( self.currentRefreshMenuInstanceDebouncedCalls[ instanceNumber ] ) {
+			clearTimeout( self.currentRefreshMenuInstanceDebouncedCalls[ instanceNumber ] );
+		}
+		self.currentRefreshMenuInstanceDebouncedCalls[ instanceNumber ] = setTimeout(
+			function() {
+				self.refreshMenuInstance( instanceNumber );
+			},
+			self.refreshDebounceDelay
+		);
+	};
+
+	return self;
+
+}( jQuery, wp.customize ) );
