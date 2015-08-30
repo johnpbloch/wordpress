b0faf1c7345d695cab812b5121cf080bdd942ff1
John P. Bloch <johnpbloch@gmail.com>
2015-08-30 07:46:15 -0400

    Update trunk

---
+++ wp-includes/comment.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Manages WordPress comments
+ *
+ * @package WordPress
+ * @subpackage Comment
+ */
+
+require_once( ABSPATH . WPINC . '/class-wp-comment-query.php' );
+require_once( ABSPATH . WPINC . '/comment-functions.php' );
