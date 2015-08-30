1e76d0d39a5696227aa762e6199f3a4141da8f59
John P. Bloch <johnpbloch@gmail.com>
2015-08-29 23:46:14 -0400

    Update trunk

---
+++ wp-includes/user.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * WordPress User API
+ *
+ * @package WordPress
+ * @subpackage Users
+ */
+
+require_once( ABSPATH . WPINC . '/user-functions.php' );
+require_once( ABSPATH . WPINC . '/class-wp-user-query.php' );
