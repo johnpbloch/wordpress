5cd4a87bd0c0e046cea3a5d811c5f1aa7f8ed6ce
John P. Bloch <johnpbloch@gmail.com>
2015-09-03 00:46:14 -0400

    Update trunk

--- wp-includes/http.php
+++ wp-includes/http.php
@@ -1,21 +1,32 @@
 <?php
 /**
- * Simple and uniform HTTP request API.
+ * Core HTTP Request API
  *
  * Standardizes the HTTP requests for WordPress. Handles cookies, gzip encoding and decoding, chunk
  * decoding, if HTTP 1.1 and various other difficult HTTP protocol implementations.
- *
- * @link https://core.trac.wordpress.org/ticket/4779 HTTP API Proposal
  *
  * @package WordPress
  * @subpackage HTTP
  * @since 2.7.0
  */
 
+/** Core HTTP API functionality */
 require_once( ABSPATH . WPINC . '/http-functions.php' );
+
+/** WP_Http class */
 require_once( ABSPATH . WPINC . '/class-http.php' );
+
+/** WP_Http_Streams class */
 require_once( ABSPATH . WPINC . '/class-wp-http-streams.php' );
+
+/** WP_Http_Curl transport class */
 require_once( ABSPATH . WPINC . '/class-wp-http-curl.php' );
+
+/** WP_HTTP_Proxy transport class */
 require_once( ABSPATH . WPINC . '/class-wp-http-proxy.php' );
+
+/** WP_Http_Cookie class */
 require_once( ABSPATH . WPINC . '/class-wp-http-cookie.php' );
+
+/** WP_Http_Encoding class */
 require_once( ABSPATH . WPINC . '/class-wp-http-encoding.php' );
