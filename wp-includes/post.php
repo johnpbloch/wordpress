bf5ce04fd56bb92abd1e8cd90d310297404e4265
John P. Bloch <johnpbloch@gmail.com>
2015-09-13 07:31:18 -0400

    Update trunk

---
+++ wp-includes/post.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Post functions and post utility function.
+ *
+ * @package WordPress
+ * @subpackage Post
+ * @since 1.5.0
+ */
+
+require_once( ABSPATH . WPINC . '/post-functions.php' );
+require_once( ABSPATH . WPINC . '/class-wp-post.php' );
