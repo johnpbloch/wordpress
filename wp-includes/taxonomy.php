c25cfd10fb0c9dc252eea5ea6cc6aebf131b2553
John P. Bloch <johnpbloch@gmail.com>
2015-09-29 12:16:18 -0400

    Update trunk

---
+++ wp-includes/taxonomy.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Core Taxonomy API
+ *
+ * @package WordPress
+ * @subpackage Taxonomy
+ * @since 2.3.0
+ */
+
+/** Core taxonomy functionality */
+require_once( ABSPATH . WPINC . '/taxonomy-functions.php' );
+
+/** WP_Tax_Query class */
+require_once( ABSPATH . WPINC . '/class-wp-tax-query.php' );
