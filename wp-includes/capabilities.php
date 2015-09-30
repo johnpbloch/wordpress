f617da3edf7464ac3dfef40f3c08bfefc1887f00
John P. Bloch <johnpbloch@gmail.com>
2015-09-30 18:46:14 -0400

    Update trunk

---
+++ wp-includes/capabilities.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Core User Role & Capabilities API
+ *
+ * @package WordPress
+ * @subpackage Users
+ * @since 2.0.0
+ */
+
+/** WP_Roles class */
+require_once( ABSPATH . WPINC . '/class-wp-roles.php' );
+
+/** WP_Role class */
+require_once( ABSPATH . WPINC . '/class-wp-role.php' );
+
+/** WP_User class */
+require_once( ABSPATH . WPINC . '/class-wp-user.php' );
+
+/** Core capabilities functionality */
+require_once( ABSPATH . WPINC . '/capabilities-functions.php' );
