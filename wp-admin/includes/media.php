de52312cd0c63ff36b908c0bb1a85958e26319ec
John P. Bloch <johnpbloch@gmail.com>
2015-10-12 12:16:14 -0400

    Update trunk

--- wp-admin/includes/media.php
+++ wp-admin/includes/media.php
@@ -106,16 +106,18 @@ function the_media_upload_tabs() {
  *
  * @since 2.5.0
  *
- * @param int     $id      image attachment id
- * @param string  $caption image caption
- * @param string  $title   image title attribute
- * @param string  $align   image css alignment property
- * @param string  $url     image src url
- * @param string  $rel     image rel attribute
- * @param string  $size    image size (thumbnail, medium, large, full or added  with add_image_size() )
- * @return string the html to insert into editor
+ * @param int     $id        Image attachment id.
+ * @param string  $caption   Image caption.
+ * @param string  $title     Image title attribute.
+ * @param string  $align     Image CSS alignment property.
+ * @param string  $url       Optional. Image src URL. Default empty.
+ * @param string  $rel       Optional. Image rel attribute. Default empty.
+ * @param string|array $size Optional. Image size. Accepts any valid image size, or an array of width
+ *                           and height values in pixels (in that order). Default 'medium'.
+ * @param string  $alt       Optional. Image alt attribute. Default empty.
+ * @return string The HTML output to insert into the editor.
  */
-function get_image_send_to_editor( $id, $caption, $title, $align, $url='', $rel = '', $size='medium', $alt = '' ) {
+function get_image_send_to_editor( $id, $caption, $title, $align, $url = '', $rel = '', $size = 'medium', $alt = '' ) {
 
 	$html = get_image_tag($id, $alt, '', $align, $size);
 
