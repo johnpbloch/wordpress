028957bf4c2cd390bc52586e894baa69dbdb4cad
John P. Bloch <johnpbloch@gmail.com>
2015-10-14 11:46:16 -0400

    Update trunk

---
+++ wp-admin/includes/template.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template WordPress Administration API.
+ *
+ * A Big Mess. Also some neat functions that are nicely written.
+ *
+ * @package WordPress
+ * @subpackage Administration
+ */
+
+/** Walker_Category_Checklist class */
+require_once( ABSPATH . 'wp-admin/includes/class-walker-category-checklist.php' );
+
+/** WP_Internal_Pointers class */
+require_once( ABSPATH . 'wp-admin/includes/class-wp-internal-pointers.php' );
+
+/** Admin template functionality */
+require_once( ABSPATH . 'wp-admin/includes/template-functions.php' );
