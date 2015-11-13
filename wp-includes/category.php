cf687b6c54378c27f08fd0373aa5f255d53ee0b1
John P. Bloch <johnpbloch@gmail.com>
2015-11-13 12:01:24 -0500

    Update trunk

---
+++ wp-includes/category.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Taxonomy API: Core category-specific functionality
+ *
+ * @package WordPress
+ * @subpackage Taxonomy
+ * @since 2.1.0
+ */
+
+/** Core category functionality */
+require_once( ABSPATH . WPINC . '/category-functions.php' );
+
+/** Walker_Category class */
+require_once( ABSPATH . WPINC . '/class-walker-category.php' );
+
+/** Walker_CategoryDropdown class */
+require_once( ABSPATH . WPINC . '/class-walker-category-dropdown.php' );
