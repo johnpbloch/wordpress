33d8920321c9b58736ba50af0d275d5255d3d429
John P. Bloch <johnpbloch@gmail.com>
2016-03-10 12:46:23 -0500

    Update trunk

--- wp-includes/general-template.php
+++ wp-includes/general-template.php
@@ -839,11 +839,11 @@ function has_site_icon( $blog_id = 0 ) {
 }
 
 /**
- * Whether the site has a custom logo.
+ * Determines whether the site has a custom logo.
  *
  * @since 4.5.0
  *
- * @param int $blog_id Optional. ID of the blog in question. Default current blog.
+ * @param int $blog_id Optional. ID of the blog in question. Default is the ID of the current blog.
  * @return bool Whether the site has a custom logo or not.
  */
 function has_custom_logo( $blog_id = 0 ) {
@@ -865,7 +865,7 @@ function has_custom_logo( $blog_id = 0 ) {
  *
  * @since 4.5.0
  *
- * @param int $blog_id Optional. ID of the blog in question. Default current blog.
+ * @param int $blog_id Optional. ID of the blog in question. Default is the ID of the current blog.
  * @return string Custom logo markup.
  */
 function get_custom_logo( $blog_id = 0 ) {
